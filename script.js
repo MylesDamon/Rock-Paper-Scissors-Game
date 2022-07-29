0c890599b1a69e6dae39f0e366295cb3f3ba4046
MylesDamon <myles.damon@hotmail.com>
2022-07-29 16:37:19 -0400

    added first function in script.js that will randomly return rock, paper, or scissors  to the console

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function getComputerChoice() {
+    let choice = ["ROCK", "PAPER", "SCISSORS"] 
+    let play = choice[Math.floor(Math.random() * choice.length)]
+   
+    console.log(play);
+    return;
+ }
+
