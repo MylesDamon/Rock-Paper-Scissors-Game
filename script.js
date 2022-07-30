bad0851f1a09963beaf62ef41feed61b77050a62
MylesDamon <myles.damon@hotmail.com>
2022-07-29 22:51:05 -0400

    made a function to run a single round of rock paper scissors (only if the user chooses rock), still not case-insensitive that's the next step

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+//let person = prompt("Choose: Rock, Paper, Or Scissors")
 
-
+// Computer's random choice;
 function getComputerChoice() {
-    let choice = ["ROCK", "PAPER", "SCISSORS"] 
-    let play = choice[Math.floor(Math.random() * choice.length)]
-   
-    console.log(play);
-    return;
+    const choice = ["ROCK", "PAPER", "SCISSORS"] 
+    return choice[Math.floor(Math.random() * choice.length)]
  }
+
+// Playing a single round that will determine winner based on what the user choose
+function round(playerSelection, computerSelection) {
+   let tie = "It's a Tie! You selected " + playerSelection + "." + " The computer selected " + computerSelection + "."
+   let youWin = "YOU WIN! You selected " + playerSelection + "." + " The computer selected " + computerSelection + "."
+   let youLose = "YOU LOSE! You selected " + playerSelection + "." + " The computer selected " + computerSelection + "."
+
+   if (playerSelection === "Rock".toUpperCase() && computerSelection === "ROCK") {
+    return tie;
+   } else if (playerSelection === "Rock".toUpperCase() && computerSelection === "PAPER") {
+    return youLose;
+   } else if (playerSelection === "Rock".toUpperCase() && computerSelection === "SCISSORS") {
+    return youWin;
+   }
+ }
+
+const playerSelection = "ROCK";
+const computerSelection = getComputerChoice()
+console.log(round(playerSelection,computerSelection))
 
