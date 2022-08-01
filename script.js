7dff8978b455fef62ff045bd126154288b1f9b32
MylesDamon <myles.damon@hotmail.com>
2022-07-31 22:52:03 -0400

    finished the function for a single round of rock,paper,scissors. Currently working on the function and for loop that will run the game 5 times.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//let person = prompt("Choose: Rock, Paper, Or Scissors")
+
 
 // Computer's random choice;
 function getComputerChoice() {
@@ -8,20 +8,64 @@ function getComputerChoice() {
 
 // Playing a single round that will determine winner based on what the user choose
 function round(playerSelection, computerSelection) {
-   let tie = "It's a Tie! You selected " + playerSelection + "." + " The computer selected " + computerSelection + "."
-   let youWin = "YOU WIN! You selected " + playerSelection + "." + " The computer selected " + computerSelection + "."
-   let youLose = "YOU LOSE! You selected " + playerSelection + "." + " The computer selected " + computerSelection + "."
+   let tie = "It's a Tie! You both selected " + computerSelection
+   let youWin = "YOU WIN! " + playerSelection + " beats " + computerSelection + "."
+   let youLose = "YOU LOSE! " + computerSelection + " beats " + playerSelection + "."
+   let result;
 
    if (playerSelection === "Rock".toUpperCase() && computerSelection === "ROCK") {
+    result = tie
     return tie;
    } else if (playerSelection === "Rock".toUpperCase() && computerSelection === "PAPER") {
+    result = youLose
     return youLose;
    } else if (playerSelection === "Rock".toUpperCase() && computerSelection === "SCISSORS") {
+    result = youWin
+    return youWIn;
+   }
+
+   if (playerSelection === "paper".toUpperCase() && computerSelection === "PAPER") {
+    result = tie
+    return tie;
+   } else if (playerSelection === "paper".toUpperCase() && computerSelection === "SCISSORS") {
+    result = youLose
+    return youLose;
+   } else if (playerSelection === "paper".toUpperCase() && computerSelection === "ROCK") {
+    result = youWin
     return youWin;
    }
+
+   if (playerSelection === "scissors".toUpperCase() && computerSelection === "SCISSORS") {
+    result = tie
+    return result;
+   } else if (playerSelection === "scissors".toUpperCase() && computerSelection === "ROCK") {
+    result = youLose
+    return youLose;
+   } else if (playerSelection === "scissors".toUpperCase() && computerSelection === "PAPER") {
+    result = youWin
+    return youWin;
+   }
+   console.log(result)
  }
 
-const playerSelection = "ROCK";
-const computerSelection = getComputerChoice()
-console.log(round(playerSelection,computerSelection))
+ //function game() {
+  //  let playerScore = 0;
+   // let computerScore = 0;
+   // for (i = 0; i < 5; i++) {
+    
+  //     const playerSelection = prompt("Choose: Rock, Paper, Or Scissors").//toUpperCase(); //Makes the user's choice case-insensitive 
+  //      const computerSelection = getComputerChoice()
+   //     round(if (result = youWin) {
+   //         playerScore += 1
+    //    } else if (result = youLose) {
+     //       computerScore += 1
+     //   })
+        
+//console.log(playerScore)
+//console.log(computerScore)
+  ///  }
+   
 
+// }
+
+// console.log(game())
