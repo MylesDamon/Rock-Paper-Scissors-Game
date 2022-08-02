8b20bd1720fb517cf382d1536aee27f5d7c1dfef
MylesDamon <myles.damon@hotmail.com>
2022-08-02 11:02:48 -0400

    Finished the entire game or Rock, Paper, Scissors. It will prompt the user to pick their choice and proceed to play 5 rounds. The consonle will tell you who won and keeps track of score.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+let playerScore = 0
+let computerScore = 0
 
 // Computer's random choice;
 function getComputerChoice() {
@@ -11,61 +12,66 @@ function round(playerSelection, computerSelection) {
    let tie = "It's a Tie! You both selected " + computerSelection
    let youWin = "YOU WIN! " + playerSelection + " beats " + computerSelection + "."
    let youLose = "YOU LOSE! " + computerSelection + " beats " + playerSelection + "."
-   let result;
+    
+
 
    if (playerSelection === "Rock".toUpperCase() && computerSelection === "ROCK") {
-    result = tie
-    return tie;
+      result = tie
+      return;
    } else if (playerSelection === "Rock".toUpperCase() && computerSelection === "PAPER") {
-    result = youLose
-    return youLose;
+     result = youLose
+     computerScore++
+     return;
    } else if (playerSelection === "Rock".toUpperCase() && computerSelection === "SCISSORS") {
-    result = youWin
-    return youWIn;
+     result = youWin
+     playerScore++
+     return;
    }
-
-   if (playerSelection === "paper".toUpperCase() && computerSelection === "PAPER") {
-    result = tie
-    return tie;
-   } else if (playerSelection === "paper".toUpperCase() && computerSelection === "SCISSORS") {
-    result = youLose
-    return youLose;
-   } else if (playerSelection === "paper".toUpperCase() && computerSelection === "ROCK") {
-    result = youWin
-    return youWin;
-   }
-
-   if (playerSelection === "scissors".toUpperCase() && computerSelection === "SCISSORS") {
-    result = tie
-    return result;
-   } else if (playerSelection === "scissors".toUpperCase() && computerSelection === "ROCK") {
-    result = youLose
-    return youLose;
-   } else if (playerSelection === "scissors".toUpperCase() && computerSelection === "PAPER") {
-    result = youWin
-    return youWin;
-   }
-   console.log(result)
- }
-
- //function game() {
-  //  let playerScore = 0;
-   // let computerScore = 0;
-   // for (i = 0; i < 5; i++) {
-    
-  //     const playerSelection = prompt("Choose: Rock, Paper, Or Scissors").//toUpperCase(); //Makes the user's choice case-insensitive 
-  //      const computerSelection = getComputerChoice()
-   //     round(if (result = youWin) {
-   //         playerScore += 1
-    //    } else if (result = youLose) {
-     //       computerScore += 1
-     //   })
-        
-//console.log(playerScore)
-//console.log(computerScore)
-  ///  }
    
 
-// }
+   if (playerSelection === "paper".toUpperCase() && computerSelection === "PAPER") {
+     result = tie
+      return;
+   } else if (playerSelection === "paper".toUpperCase() && computerSelection === "SCISSORS") {
+     result = youLose
+     computerScore++
+     return;
+   } else if (playerSelection === "paper".toUpperCase() && computerSelection === "ROCK") {
+     result = youWin
+     playerScore++
+     return;
+   }
+   
 
-// console.log(game())
+   if (playerSelection === "scissors".toUpperCase() && computerSelection === "SCISSORS") {
+     result = tie
+     return;
+   } else if (playerSelection === "scissors".toUpperCase() && computerSelection === "ROCK") {
+     result = youLose
+     computerScore++
+     return;
+   } else if (playerSelection === "scissors".toUpperCase() && computerSelection === "PAPER") {
+     result = youWin
+     playerScore++
+     return;
+   }
+    
+}
+
+ function game() {
+    for (i = 0; i < 5; i++) {
+    
+    const playerSelection = prompt("Choose: Rock, Paper, Or Scissors").toUpperCase(); //Makes the user's choice case-insensitive 
+    const computerSelection = getComputerChoice()   
+   
+       
+
+    round(playerSelection,computerSelection)
+    console.log(result)
+    console.log("Player Score: " + playerScore)
+    console.log("Computer Score: " + computerScore)  
+    }
+
+}
+
+ console.log(game())
